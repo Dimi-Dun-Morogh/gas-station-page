ac47e2d1022deae48496217d3296d5dae522c36b
Dimi-Dun-Morogh <darnelbro@gmail.com>
2020-07-23 22:18:41 +0300

    header section + slider in content section

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(function(){
+    //slider
+    $(".slider").slick({
+      arrows: false,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 10000,
+      fade: true,
+      // responsive: [
+      //   {
+      //     breakpoint: 601,
+      //     settings: {
+      //       // arrows: false,
+      //       // slidesToShow: 3,
+      //       dots: false,
+      //     },
+      //   },
+      // ],
+    });
+})
